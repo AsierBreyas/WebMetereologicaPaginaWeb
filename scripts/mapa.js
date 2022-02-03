0f06d0e484149fbe0624c58b05d909418718b42b
AsierBreyas <72077655+AsierBreyas@users.noreply.github.com>
2022-02-03 14:02:19 +0100

    Web metida en el servidor, faltan cosas

--- scripts/mapa.js
+++ scripts/mapa.js
@@ -25,7 +25,7 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 L.control.scale().addTo(mymap);
 fetch('https://localhost:5001/api/Tiempo').then(response => response.json()).then(registroTiempo => {
-    aPrueba = registroTiempo;
+    var aPrueba = registroTiempo;
     aPrueba.forEach(element => {
         var localidad = { 'localidad': element.municipio, 'Temperatura': true, 'Viento': false, 'Tiempo': true, 'Precipitacion': false };
         var marker = L.marker([parseFloat((element.latitud).replace(',', '.')), parseFloat((element.longitud).replace(',', '.'))]).on('click', function () { Añadir(localidad) });
@@ -53,7 +53,7 @@ function activarDrop(id){
     $(`#${id}`).droppable({
         drop: function (event, ui) {
             var opcion = $(ui.draggable).attr("alt");
-            localidad = JSON.parse(localStorage.getItem(id));
+            var localidad = JSON.parse(localStorage.getItem(id));
             localidad[opcion] = true;
             localStorage.setItem(localidad.localidad ,JSON.stringify(localidad));
             hacerOpciones(opcion, id);
@@ -74,9 +74,9 @@ function activarBotones() {
         $("#campos").html() === '' ? $("#campos").html(sNoCampos) : "";
     });
     $(`.opcion`).on('click', function(){
-        opcion = $(this).attr('id');
-        id = $(this).attr('alt');
-        localidad = JSON.parse(localStorage.getItem(id));
+        var opcion = $(this).attr('id');
+        var id = $(this).attr('alt');
+        var localidad = JSON.parse(localStorage.getItem(id));
         localidad[opcion] = false;
         localStorage.setItem(localidad.localidad ,JSON.stringify(localidad));
         hacerOpciones(opcion, id);
@@ -85,12 +85,12 @@ function activarBotones() {
 }
 function GenerarDatos(Codigo){
     fetch(`https://localhost:5001/api/Tiempo/${Codigo}`).then(response => response.json()).then(unRegistroTiempo => {
-        sDatoNuevo = `<div class="card col " id="${Codigo}">  <div class="card-body>"> <h4 class="card-title"> ${unRegistroTiempo["municipio"]}</h4>`;
+        var sDatoNuevo = `<div class="card col " id="${Codigo}">  <div class="card-body>"> <h4 class="card-title"> ${unRegistroTiempo["municipio"]}</h4>`;
         sDatoNuevo += `<button class="btn-close" id="btn${Codigo}" value="${Codigo}" aria-label="Cerrar pestaña"></button>`;
         sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Temperatura'><h5 class="bi bi-thermometer" role="img" alt="Temperatura"> ${unRegistroTiempo["temperatura"]} ºC </h5><h5 class='bi bi-x-octagon opcion' id="Temperatura" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
         sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Viento'><h5 class="bi bi-wind"  role="img" alt="Viento"> ${unRegistroTiempo["velocidadViento"]} km/h </h5><h5 class='bi bi-x-octagon opcion' id="Viento" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
         sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Tiempo'><h5 class="bi bi-cloud"  role="img" alt="Tiempo"> <img src='${unRegistroTiempo["pathImg"]}'></img></h5><h5 class='bi bi-x-octagon opcion' id="Tiempo" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
-        sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Precipitacion'><h5 class="bi bi-umbrella" role="img" alt="Precipitacion"> ${unRegistroTiempo["precipitacionAcumulada"]}% </h5><h5 class='bi bi-x-octagon opcion' id="Precipitacion" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`
+        sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Precipitacion'><h5 class="bi bi-umbrella" role="img" alt="Precipitacion"> ${unRegistroTiempo["precipitacion"]}% </h5><h5 class='bi bi-x-octagon opcion' id="Precipitacion" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`
         sDatoNuevo += "</div></div>";
         ponerCartas(sDatoNuevo, Codigo);
     });
