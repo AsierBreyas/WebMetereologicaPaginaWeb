e1215a15701b63efc07e0c2ae7dd5217242d7057
AsierBreyas <72077655+AsierBreyas@users.noreply.github.com>
2022-02-04 13:07:40 +0100

    A

--- scripts/mapa.js
+++ scripts/mapa.js
@@ -24,15 +24,15 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 }).addTo(mymap);
 L.control.scale().addTo(mymap);
-fetch('https://localhost:5001/api/Tiempo').then(response => response.json()).then(registroTiempo => {
+fetch('http://10.10.17.170/api').then(response => response.json()).then(registroTiempo => {
     var aPrueba = registroTiempo;
     aPrueba.forEach(element => {
-        var localidad = { 'localidad': element.municipio, 'Temperatura': true, 'Viento': false, 'Tiempo': true, 'Precipitacion': false };
+        var localidad = { 'localidad': element.municipio, 'Temperatura': true, 'Viento': false, 'Tiempo': true, 'Precipitacion': false, 'Humedad': false };
         var marker = L.marker([parseFloat((element.latitud).replace(',', '.')), parseFloat((element.longitud).replace(',', '.'))]).on('click', function () { Añadir(localidad) });
         group.addLayer(marker);
         element.marker_id = group.getLayerId(marker);
         //Una vez terminado el mapa vamos a mirar si en el localStorage está algo guardado y lo metemos en el HTML
-        localStorage.getItem(element.municipio) !=null ? GenerarDatos(element.municipio) : "";
+        localStorage.getItem(element.municipio) != null ? GenerarDatos(element.municipio) : "";
     });
 })
 group.addTo(mymap);
@@ -41,26 +41,69 @@ $(".draggable").draggable({
     zIndex: 100,
     revert: true,
     revertDuration: 0,
-    start: function(event, ui){
+    start: function (event, ui) {
         $('.ui-droppable').css('background-color', '#19875')
     },
-    stop: function(event, ui){
+    stop: function (event, ui) {
         $('.ui-droppable').css('background-color', '#939393')
     }
 });
 $("#campos").html() === '\n\n\n            ' ? $("#campos").html(sNoCampos) : "";
-function activarDrop(id){
+localStorage.getItem('modo') === 'oscuro' ? modoOscuro() : modoClaro();
+$('#cambiaColor').on('click', function(){cambiarColores()});
+function activarDrop(id) {
     $(`#${id}`).droppable({
         drop: function (event, ui) {
             var opcion = $(ui.draggable).attr("alt");
             var localidad = JSON.parse(localStorage.getItem(id));
             localidad[opcion] = true;
-            localStorage.setItem(localidad.localidad ,JSON.stringify(localidad));
+            localStorage.setItem(localidad.localidad, JSON.stringify(localidad));
             hacerOpciones(opcion, id);
         }
     });
 }
+function cambiarColores() {
+    if (localStorage.getItem('modo') === 'oscuro') {
+        localStorage.setItem('modo', 'claro');
+        modoClaro();
+    } else if (localStorage.getItem('modo') === 'claro') {
+        localStorage.setItem('modo', 'oscuro');
+        modoOscuro();
+    } else {
+        localStorage.setItem('modo', 'claro');
+    }
+}
+function modoClaro(){
+    $('.navbar').removeClass('navbar-dark');
+    $('.navbar').removeClass('bg-dark');
+    $('.navbar').addClass('navbar-light');
+    $('.navbar').addClass('bg-light');
+    $('body').css('background-color', 'rgb(200, 200, 200)');
+    $('h3').css('color','rgb(33, 37, 41)');
+    $('h4').css('color','rgb(33, 37, 41)');
+    $('h5').css('color','rgb(33, 37, 41)');
+    $('.iconoJackson').removeClass('bi-moon');
+    $('.iconoJackson').addClass(' bi-brightness-high-fill');
+    $('#map').css('box-shadow', 'rgb(161, 161, 161) 5px 5px 5px 0px');
+    $('.div').css('border','2px solid rgb(71, 71, 71)');
+    $('.card').css('background-color', 'rgb(147, 147, 147)');
+}
 
+function modoOscuro(){
+    $('.navbar').removeClass('navbar-light');
+    $('.navbar').removeClass('bg-light');
+    $('.navbar').addClass('navbar-dark');
+    $('.navbar').addClass('bg-dark');
+    $('.iconoJackson').removeClass(' bi-brightness-high-fill');
+    $('.iconoJackson').addClass('bi-moon');
+    $('body').css('background-color', 'rgb(55, 55,55 )');
+    $('h3').css('color', 'rgb(184, 188, 192)');
+    $('h4').css('color', 'rgb(184, 188, 192)');
+    $('h5').css('color', 'rgb(184, 188, 192)');
+    $('#map').css('box-shadow','rgb(80, 80, 80) 5px 5px 5px 0px');
+    $('.div').css('border','2px solid rgb(121, 121, 121)');
+    $('.card').css('background-color', 'rgb(77, 77, 77)');
+}
 function Añadir(oLocalidad) {
     // var localidad = { 'localidad': Nombre, 'Temperatura': true, 'Viento': false, 'Tiempo': true, 'Precipitacion': false };
     localStorage.getItem(oLocalidad.localidad) === null ? localStorage.setItem(oLocalidad.localidad, JSON.stringify(oLocalidad)) : localStorage.removeItem(oLocalidad.localidad);
@@ -73,40 +116,42 @@ function activarBotones() {
         localStorage.removeItem(this.value);
         $("#campos").html() === '' ? $("#campos").html(sNoCampos) : "";
     });
-    $(`.opcion`).on('click', function(){
+    $(`.opcion`).on('click', function () {
         var opcion = $(this).attr('id');
         var id = $(this).attr('alt');
         var localidad = JSON.parse(localStorage.getItem(id));
         localidad[opcion] = false;
-        localStorage.setItem(localidad.localidad ,JSON.stringify(localidad));
+        localStorage.setItem(localidad.localidad, JSON.stringify(localidad));
         hacerOpciones(opcion, id);
         activarDrop(id);
     });
 }
-function GenerarDatos(Codigo){
-    fetch(`https://localhost:5001/api/Tiempo/${Codigo}`).then(response => response.json()).then(unRegistroTiempo => {
+function GenerarDatos(Codigo) {
+    fetch(`http://10.10.17.170/api/${Codigo}`).then(response => response.json()).then(unRegistroTiempo => {
         var sDatoNuevo = `<div class="card col " id="${Codigo}">  <div class="card-body>"> <h4 class="card-title"> ${unRegistroTiempo["municipio"]}</h4>`;
         sDatoNuevo += `<button class="btn-close" id="btn${Codigo}" value="${Codigo}" aria-label="Cerrar pestaña"></button>`;
+        sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Tiempo'><h5 class="bi bi-cloud"  role="img" alt="Tiempo"> ${unRegistroTiempo["descripcion"]} </h5><h5 class='bi bi-x-octagon opcion' id="Tiempo" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
         sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Temperatura'><h5 class="bi bi-thermometer" role="img" alt="Temperatura"> ${unRegistroTiempo["temperatura"]} ºC </h5><h5 class='bi bi-x-octagon opcion' id="Temperatura" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
         sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Viento'><h5 class="bi bi-wind"  role="img" alt="Viento"> ${unRegistroTiempo["velocidadViento"]} km/h </h5><h5 class='bi bi-x-octagon opcion' id="Viento" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
-        sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Tiempo'><h5 class="bi bi-cloud"  role="img" alt="Tiempo"> <img src='${unRegistroTiempo["pathImg"]}'></img></h5><h5 class='bi bi-x-octagon opcion' id="Tiempo" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
+        sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Humedad'><h5 class="bi bi-water"  role="img" alt="Humedad"> ${unRegistroTiempo["humedad"]} %</h5><h5 class='bi bi-x-octagon opcion' id="Humedad" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`;
         sDatoNuevo += `<div class="opciones" id='${unRegistroTiempo["municipio"]}Precipitacion'><h5 class="bi bi-umbrella" role="img" alt="Precipitacion"> ${unRegistroTiempo["precipitacion"]}% </h5><h5 class='bi bi-x-octagon opcion' id="Precipitacion" role="img" alt="${unRegistroTiempo["municipio"]}"></h5></div>`
         sDatoNuevo += "</div></div>";
         ponerCartas(sDatoNuevo, Codigo);
     });
 }
-function ponerCartas(html, Codigo){
-    $("#campos").html() === sNoCampos ? $("#campos").html(html): $("#campos").html($("#campos").html() + html);
+function ponerCartas(html, Codigo) {
+    $("#campos").html() === sNoCampos ? $("#campos").html(html) : $("#campos").html($("#campos").html() + html);
     hacerOpciones("Temperatura", Codigo);
     hacerOpciones("Viento", Codigo);
     hacerOpciones("Tiempo", Codigo);
     hacerOpciones("Precipitacion", Codigo);
+    hacerOpciones("Humedad", Codigo);
     activarBotones();
     activarDrop(Codigo);
 }
-function hacerOpciones(opcion, Codigo){
+function hacerOpciones(opcion, Codigo) {
     var refLocalidad = JSON.parse(localStorage.getItem(Codigo));
-    refLocalidad[opcion] === false ? $(`#${Codigo}${opcion}`).hide(): $(`#${Codigo}${opcion}`).show();
+    refLocalidad[opcion] === false ? $(`#${Codigo}${opcion}`).hide() : $(`#${Codigo}${opcion}`).show();
 }
 function EliminarHtml(Codigo) {
     $(`#${Codigo}`).remove();
